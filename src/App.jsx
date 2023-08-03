2d523c569e12f5646e60d3eecffdf554ff51b9ae
Apeksha <apekshashetti1012@gmail.com>
2023-08-03 19:25:47 +0530

    feature component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
-export default function App() {
-  return <div>App</div>;
+import Header from "./components/Header";
+import "./App.css";
+import NotesListPage from "./pages/NotesListPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <NotesListPage />
+    </div>
+  );
 }
+
+export default App;
