5e2675a1024c53de889fa40cf860709769d10dd8
Apeksha <apekshashetti1012@gmail.com>
2023-08-05 15:28:01 +0530

    ref : update

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import { AiOutlinePlus } from "react-icons/ai";
+
+const AddButton = () => {
+  return (
+    <Link to="/note/new" className="floating-button">
+      <AiOutlinePlus />
+    </Link>
+  );
+};
+
+export default AddButton;
